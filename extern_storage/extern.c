1b5eddee550c09234ce14316b2484d4800df50d6
Tomislav Kraljic <63562144+TomislavKraljic@users.noreply.github.com>
2021-05-18 04:21:02 -0400

    Add files via upload

---
+++ extern_storage/extern.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int x = 32;
+
+int b = 8;
+
+void print_x(){ printf("The value of extern variable x is: %d\n", x); }
+
+void print_b(){ printf("The value of extern variable b is: %d\n", b); }
